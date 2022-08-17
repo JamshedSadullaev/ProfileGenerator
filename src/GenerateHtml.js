78046d20222d5ab51d082e108f1565ec0028a0ba
JamshedSadullaev <sadullayev98@gmail.com>
2022-08-17 19:12:39 -0400

    removed node_modules

--- src/GenerateHtml.js
+++ src/GenerateHtml.js
@@ -1,2 +1,28 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+
+
+function htmlTemplate(data){
+return  `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+${data}
+</body>
+</html>`
+
+}
+
+
+    
+
+
+fs.writeFile('1.html', htmlTemplate('<h1>hello</h1>s'), function(){
+    console.log('test')
+});
