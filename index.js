89101c02e389b3c8a8b3da99aee4b618cebe1e68
JamshedSadullaev <sadullayev98@gmail.com>
2022-08-20 16:45:59 -0400

    Adding index.js

--- index.js
+++ index.js
@@ -1,2 +1,97 @@
-const fs= require("fs/promises");
+const fs= require("fs");
 const inquirer =require("inquirer");
+const Manager = require("./lib/Manager");
+const Engineer = require ("./lib/Engineer");
+const Intern = require ("./lib/Intern");
+
+const util = require("util");
+const generateSite  = require("./src/GenerateHtml");
+const writeFileAsync = util.promisify(fs.writeFile);
+
+let teamArray = [];
+let teamString =``;
+// function to generate question 
+async function main () {
+    try{
+        await prompt()
+        for (let i = 0; i<teamArray.length; i++){
+            teamString = teamString + generateSite.generateTemplate(teamArray[i]);
+        }
+        let finalHtml = generateSite.generateHtml(teamString);
+        writeFileAsync('./dist/index.html',finalHtml);
+        console.log();
+        console.log("Profile has been created succesfully")
+    }catch (err) {
+        return console.log(err);
+    }
+}
+
+async function prompt () {
+    // question goes here 
+    let genQuestion = "";
+    do{
+        try{
+            let response = await inquirer.prompt([
+                {
+                    type:'input',
+                    name:'name',
+                    message:'What is your name?',
+                },
+                {
+                    type:'input',
+                    name:'id',
+                    message:'What is your ID?',
+                },
+                {
+                    type:'input',
+                    name:'email',
+                    message:'What is your email?',
+                },
+                {
+                    type:'list',
+                    name:'role',
+                    message:'What is your role?',
+                    choices:["Engineer","Intern","Manager"],
+                },
+            ]);
+            let response2 =""
+            if(response.role==="Engineer"){
+                response2 = await inquirer.prompt([{
+                    type:'input',
+                    name:'name',
+                    message:'What is the employees github name',
+                }, ]);
+                const engineer = new Engineer(response.name,response.id,response.email,response2.name);
+                teamArray.push(engineer);
+            }else if (response.role ==="Manager"){
+                response2 =await inquirer.prompt ([{
+                    type:'input',
+                    name:'name',
+                    message:'What is the office number?',
+                }, ]);
+                const manager = new Manager(response.name,response.id,response.email,response2.name);
+                teamArray.push(manager);
+            }else if (response.role ==="Intern"){
+                response2 =await inquirer.prompt ([{
+                    type:'input',
+                    name:'name',
+                    message:'What is your school name?',
+                },]);
+                const intern = new Intern (response.name,response.id,response.email,response2.name);
+                teamArray.push(intern);
+            }
+        }catch(err){
+            return console.log(err);
+        }
+        genQuestion=await inquirer.prompt  ([{
+            type:'list',
+            name:'finish',
+            message:'Do you want to continue?',
+            choices:['Yes','No']
+        },]);
+    }while(genQuestion.finish ==="Yes");
+
+
+}
+// returning the function to start a question
+main();
--- src/GenerateHtml.js
+++ src/GenerateHtml.js
@@ -1,28 +1,82 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-
-
-
-function htmlTemplate(data){
-return  `<!DOCTYPE html>
+const generateHtml = function (teamString) {
+    return `<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>ProfileGenerator</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
-<body>
-${data}
-</body>
-</html>`
+<div class="header">
+    <div class="p-3 mb-2 bg-danger text-white d-flex justify-content-center">
+        <h1>My Team</h1>
+        
+    </div>
+</div>
+<div class="container ">
+    <div class="row d-flex justify-content-around">
+        ${teamString}
+</div>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cup-hot" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M.5 6a.5.5 0 0 0-.488.608l1.652 7.434A2.5 2.5 0 0 0 4.104 16h5.792a2.5 2.5 0 0 0 2.44-1.958l.131-.59a3 3 0 0 0 1.3-5.854l.221-.99A.5.5 0 0 0 13.5 6H.5ZM13 12.5a2.01 2.01 0 0 1-.316-.025l.867-3.898A2.001 2.001 0 0 1 13 12.5ZM2.64 13.825 1.123 7h11.754l-1.517 6.825A1.5 1.5 0 0 1 9.896 15H4.104a1.5 1.5 0 0 1-1.464-1.175Z"/>
+        <path d="m4.4.8-.003.004-.014.019a4.167 4.167 0 0 0-.204.31 2.327 2.327 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.593.593 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3.31 3.31 0 0 1-.202.388 5.444 5.444 0 0 1-.253.382l-.018.025-.005.008-.002.002A.5.5 0 0 1 3.6 4.2l.003-.004.014-.019a4.149 4.149 0 0 0 .204-.31 2.06 2.06 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.593.593 0 0 0-.09-.252A4.334 4.334 0 0 0 3.6 2.8l-.01-.012a5.099 5.099 0 0 1-.37-.543A1.53 1.53 0 0 1 3 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a5.446 5.446 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 4.4.8Zm3 0-.003.004-.014.019a4.167 4.167 0 0 0-.204.31 2.327 2.327 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.593.593 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3.31 3.31 0 0 1-.202.388 5.444 5.444 0 0 1-.253.382l-.018.025-.005.008-.002.002A.5.5 0 0 1 6.6 4.2l.003-.004.014-.019a4.149 4.149 0 0 0 .204-.31 2.06 2.06 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.593.593 0 0 0-.09-.252A4.334 4.334 0 0 0 6.6 2.8l-.01-.012a5.099 5.099 0 0 1-.37-.543A1.53 1.53 0 0 1 6 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a5.446 5.446 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 7.4.8Zm3 0-.003.004-.014.019a4.077 4.077 0 0 0-.204.31 2.337 2.337 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.593.593 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3.198 3.198 0 0 1-.202.388 5.385 5.385 0 0 1-.252.382l-.019.025-.005.008-.002.002A.5.5 0 0 1 9.6 4.2l.003-.004.014-.019a4.149 4.149 0 0 0 .204-.31 2.06 2.06 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.593.593 0 0 0-.09-.252A4.334 4.334 0 0 0 9.6 2.8l-.01-.012a5.099 5.099 0 0 1-.37-.543A1.53 1.53 0 0 1 9 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a5.446 5.446 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 10.4.8Z"/>
+      </svg>
+</div>
+        
+        
 
+     
+
+       
+
+</html>
+`
+
+
+}
+const generateTemplate = function (arr) {
+    let icon;
+    let info;
+    if (arr.title === "Manager") {
+        icon = `<i class="bi bi-cup-hot"></i>`
+        info = `Office number: ${arr.officeNumber}`
+
+    } else if (arr.title === "Engineer") {
+        icon = `<i class="bi bi-eyeglasses"></i>`
+        info = `Github: <a href="https://www.github.com">${arr.github}</a>`
+    } else if (arr.title === "Intern") {
+        icon = `<i class="bi bi-mortarboard"></i>`
+        info = `School:${arr.school}`
+    }
+    return `
+
+        <div class="card" style="width: 18rem;">
+    <div class="card-body p-3 mb-2 bg-primary text-white">
+    <h4 class="card-title">${arr.name}</h4>
+  <h4 class="card-title">${icon}</i> ${arr.title}</h4>
+</div>
+<div class="card-body card-header">
+    <div class="card-body list-group list-group-flush">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${arr.id}</li>
+            <li class="list-group-item"><a href="https://gmail.com/">Email:${arr.email}</a></li>
+            <li class="list-group-item">${info}</li>
+          </ul>
+
+    </div>
+</div>
+</div>
+
+    `
 }
 
 
-    
-
-
-fs.writeFile('1.html', htmlTemplate('<h1>hello</h1>s'), function(){
-    console.log('test')
-});
+exports.generateHtml = generateHtml;
+exports.generateTemplate = generateTemplate;
+// module.exports = generateHtml;
+// module.exports =generateTemplate;
